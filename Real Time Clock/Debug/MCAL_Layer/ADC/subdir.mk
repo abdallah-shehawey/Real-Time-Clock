a088dd18752d4bbebe3e1ee2f251da4784e89bcb
abdallah-shehawey <shehawey9@gmail.com>
2024-08-27 02:17:49 +0300

    Real Time Clock - AVR Project

--- Real Time Clock/Debug/MCAL_Layer/ADC/subdir.mk
+++ Real Time Clock/Debug/MCAL_Layer/ADC/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 MCAL_Layer/ADC/%.o: ../MCAL_Layer/ADC/%.c MCAL_Layer/ADC/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: AVR Compiler'
-	avr-gcc -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega16 -DF_CPU=1000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	avr-gcc -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega32 -DF_CPU=8000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
