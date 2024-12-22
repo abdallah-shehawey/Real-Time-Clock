a96be9a037a3d1c8ec5d92116447de79161a44bd
abdallah-shehawey <shehawey9@gmail.com>
2024-12-22 20:51:02 +0200

    Real Time Clock - AVR Project

--- Real Time Clock/Debug/APP_Layer/Real Time Clock.d
+++ Real Time Clock/Debug/APP_Layer/Real Time Clock.d
@@ -3,7 +3,7 @@ APP_Layer/Real Time Clock.o APP_Layer/Real\ Time\ Clock.o:  \
   ../APP_Layer/STD_TYPES.h \
   ../APP_Layer/../MCAL_Layer/TIMER/TIMER_interface.h \
   ../APP_Layer/../MCAL_Layer/DIO/DIO_interface.h \
-  ../APP_Layer/../MCAL_Layer/DIO/DIO_private.h \
+  ../APP_Layer/../MCAL_Layer/DIO/DIO_private.h\ \  \
   ../APP_Layer/../MCAL_Layer/GIE/GIE_interface.h \
   ../APP_Layer/../MCAL_Layer/ADC/ADC_interface.h \
   ../APP_Layer/../HAL_Layer/BCD_TO_SSD/BCD_To_SSD_interface.h \
@@ -20,7 +20,7 @@ APP_Layer/Real Time Clock.o APP_Layer/Real\ Time\ Clock.o:  \
 
 ../APP_Layer/../MCAL_Layer/DIO/DIO_interface.h:
 
-../APP_Layer/../MCAL_Layer/DIO/DIO_private.h:
+../APP_Layer/../MCAL_Layer/DIO/DIO_private.h\ \ :
 
 ../APP_Layer/../MCAL_Layer/GIE/GIE_interface.h:
 
